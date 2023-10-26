e813e7331a4bd29e271aa6379354c131dec086c1
William35er <125413709+William35er@users.noreply.github.com>
2023-10-26 00:33:45 +0000

    register-react-flask-Authen-completed

--- src/api/routes.py
+++ src/api/routes.py
@@ -4,7 +4,7 @@ This module takes care of starting the API Server, Loading the DB and Adding the
 from flask import Flask, request, jsonify, url_for, Blueprint
 from api.models import db, User
 from api.utils import generate_sitemap, APIException
-
+import hashlib
 api = Blueprint('api', __name__)
 
 
@@ -16,3 +16,18 @@ def handle_hello():
     }
 
     return jsonify(response_body), 200
+
+@api.route('/signup', methods=['POST'])
+def create_account():
+
+    response_body = request.get_json()
+    email = response_body.get("email")
+    password = response_body.get("password")
+    user = User.query.filter_by(email = email).first()
+    if user is not None:
+        return jsonify("account already exist"), 403
+    hash_password = hashlib.sha224(password.encode("utf-8")).hexdigest()
+    new_user = User(email = email, password = hash_password)
+    db.session.add(new_user)
+    db.sessiom.commit()
+    return jsonify("account has been successfully created"), 200
