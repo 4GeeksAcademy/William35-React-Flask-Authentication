8e686217a2eab3ab88974c97e156f42fcf764be7
William35er <125413709+William35er@users.noreply.github.com>
2023-10-31 00:13:43 +0000

    changemade-to-flaskauthenapi

--- src/api/routes.py
+++ src/api/routes.py
@@ -5,6 +5,7 @@ from flask import Flask, request, jsonify, url_for, Blueprint
 from api.models import db, User
 from api.utils import generate_sitemap, APIException
 import hashlib
+from flask_jwt_extended import create_access_token, get_jwt_identity, jwt_required
 api = Blueprint('api', __name__)
 
 
@@ -29,5 +30,27 @@ def create_account():
     hash_password = hashlib.sha224(password.encode("utf-8")).hexdigest()
     new_user = User(email = email, password = hash_password)
     db.session.add(new_user)
-    db.sessiom.commit()
+    db.session.commit()
     return jsonify("account has been successfully created"), 200
+
+@api.route('/login', methods=['POST'])
+def access_account():
+
+    response_body = request.get_json()
+    email = response_body.get("email")
+    password = response_body.get("password")
+    user = User.query.filter_by(email = email).first()
+    if user is None:
+        return jsonify("account does not exist"), 400
+    hash_password = hashlib.sha224(password.encode("utf-8")).hexdigest()
+    user = User.query.filter_by(email = email, password = hash_password).first()
+    if user is None:
+        return jsonify("invald password"), 401
+    token = create_access_token(identity = user.id)
+    return jsonify(token = token), 200
+
+@api.route('/authorize', methods=['GET'])
+@jwt_required()
+def verify_account():
+    user_id = get_jwt_identity()
+    return jsonify(user = user_id), 200
---
+++ src/front/js/pages/private.js
@@ -0,0 +1,5 @@
+import React from "react"
+function Private(){
+    return <h1>Hello World</h1>
+}
+export default Private
